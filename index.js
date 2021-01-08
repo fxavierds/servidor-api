3dc5a646d6479f245e784dfbd6934fc4ffa568fb
fxavierds <fxavierds@yahoo.com.br>
2021-01-07 22:54:51 -0300

    upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const { listarTarefaId,
         listarTarefas,
@@ -10,11 +11,17 @@ const { listarTarefaId,
         concluirTarefa
  } = require('./controllers/gerenciador-tarefas.js');
 
+ const {
+    finalizarCompra, obterCidadesPorEstado
+  } = require('./controllers/mini-ecommerce.js');
+
 const app = express();
 const port = 3001;
 
+app.use(express.static('public'));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(fileUpload({ createParentPath: true }));
 
 // listar todas as tarefas - get
 app.get('/gerenciador-tarefas', listarTarefas);
@@ -28,5 +35,9 @@ app.put('/gerenciador-tarefas/:id', atualizarTarefa);
 app.delete('/gerenciador-tarefas/:id', removerTarefa);
 // concluir uma tarefa - put
 app.put('/gerenciador-tarefas/:id/concluir', concluirTarefa);
+
+// mini-ecommerce
+app.post('/mini-ecommerce/checkout/finalizar-compra', finalizarCompra);
+app.get('/mini-ecommerce/estado/:siglaEstado/cidades', obterCidadesPorEstado);
 
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
